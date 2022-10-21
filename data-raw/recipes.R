78ae71ee01cdc5c5433a225758dac56bde400994
Andrew Goldstone <andrew.goldstone@gmail.com>
2022-10-21 14:55:25 -0400

    document Karsdorp et al. download

--- data-raw/recipes.R
+++ data-raw/recipes.R
@@ -1,5 +1,18 @@
 library(tidyverse)
+library(curl)
 
-recipes <- read_csv("karsdorp-et-al/01-introduction-cook-books/data/feeding-america.csv")
+zipfile <- "karsdorp-et-al.zip"
+
+# Fetch the datasets for Karsdorp et al.'s  Humanities Data Analysis, stored at
+# doi:10.5281/zenodo.891264
+
+curl_download("https://zenodo.org/record/3560761/files/data.zip?download=1",
+              destfile=zipfile)
+
+unzip(zipfile,
+      files="01-introduction-cook-books/data/feeding-america.csv",
+      junkpaths=T)
+
+recipes <- read_csv("feeding-america.csv")
 recipes <- recipes |> mutate(ingredients=str_split(ingredients,";"))
 usethis::use_data(recipes, overwrite = TRUE)
